64d4bb9705f8c78614b3b48e8597dc7a3b9588e5
Martin Marianetti <martin.marianetti@gmail.com>
2017-08-06 14:23:14 -0300

    searchNodeAt

--- structures/linked-list.js
+++ structures/linked-list.js
@@ -1,113 +1,124 @@
 class ListNode {
 
-    constructor(data) {
-        this.data = data;
-    }
+  constructor(data) {
+    this.data = data;
+  }
 }
 
 class LinkedList {
 
-    constructor() {
-        this.firstElement = undefined;
-        this.lastElement = undefined;
+  constructor() {
+    this.firstElement = undefined;
+    this.lastElement = undefined;
+  }
+
+  add(element) {
+
+    let node = new ListNode(element);
+
+    if (!this.firstElement) {
+      this._addFirstElementOfList(node);
+    } else {
+      this._addToEnd(node);
+    }
+  }
+
+  _addFirstElementOfList(node) {
+    this.firstElement = node;
+    this.lastElement = node;
+  }
+
+  _addToEnd(node) {
+    this.lastElement.next = node;
+    this.lastElement = node;
+  }
+
+  nodeCount() {
+    let tempElement = this.firstElement,
+      count = 0;
+    while (tempElement) {
+      count++;
+      tempElement = tempElement.next;
+    }
+    return count;
+  }
+
+  addFirst(element) {
+    let node = new ListNode(element);
+
+    if (!this.firstElement) {
+      this._addFirstElementOfList(node);
+    } else {
+      let temp = this.firstElement;
+      this.firstElement = node;
+      this.firstElement.next = temp;
+    }
+  }
+
+  _findElement(element) {
+    let found,
+      tempElement = this.firstElement;
+    while (tempElement.next) {
+
+      if (tempElement.data === element) {
+        found = tempElement;
+        break;
+      } else {
+        tempElement = tempElement.next;
+
+      }
+    }
+    return found;
+  }
+
+  searchNodeAt(position) {
+    let currentPos = 0,
+      tempElement = this.firstElement;
+
+    while (currentPos < position) {
+      tempElement = tempElement.next;
+      currentPos++;
+    }
+    return tempElement;
+  }
+
+  deleteNode(element) {
+    let found,
+      tempElement = this.firstElement;
+    while (tempElement.next) {
+
+      if (tempElement.next.data === element) {
+        found = tempElement;
+        break;
+      } else {
+        tempElement = tempElement.next;
+      }
     }
 
-    add(element) {
+    let elementToDelete = found.next;
 
-        let node = new ListNode(element);
-
-        if (!this.firstElement) {
-            this._addFirstElementOfList(node);
-        } else {
-            this._addToEnd(node);
-        }
+    if (elementToDelete.next) {
+      found.next = elementToDelete.next;
+    } else {
+      found.next = null;
+      this.lastElement = found;
     }
 
-    _addFirstElementOfList(node) {
-        this.firstElement = node;
-        this.lastElement = node;
+  }
+
+  print() {
+    let tempElement = this.firstElement;
+    while (tempElement) {
+      console.log("--------------------");
+      console.log("NODE:" + tempElement.data);
+      let next = (tempElement.next && tempElement.next.data)
+        ? tempElement.next.data
+        : null;
+      console.log('-> next: ' + next);
+      console.log("--------------------");
+      tempElement = tempElement.next;
     }
-
-    _addToEnd(node) {
-        this.lastElement.next = node;
-        this.lastElement = node;
-    }
-
-    nodeCount() {
-        let tempElement = this.firstElement,
-            count = 0;
-        while (tempElement) {
-            count++;
-            tempElement = tempElement.next;
-        }
-        return count;
-    }
-
-    addFirst(element) {
-        let node = new ListNode(element);
-
-        if (!this.firstElement) {
-            this._addFirstElementOfList(node);
-        } else {
-            let temp = this.firstElement;
-            this.firstElement = node;
-            this.firstElement.next = temp;
-        }
-    }
-
-    _findElement(element) {
-        let found,
-            tempElement = this.firstElement;
-        while (tempElement.next) {
-
-            if (tempElement.data === element) {
-                found = tempElement;
-                break;
-            } else {
-                tempElement = tempElement.next;
-
-            }
-        }
-        return found;
-    }
-
-    deleteNode(element) {
-        let found,
-            tempElement = this.firstElement;
-        while (tempElement.next) {
-
-            if (tempElement.next.data === element) {
-                found = tempElement;
-                break;
-            } else {
-                tempElement = tempElement.next;
-            }
-        }
-
-        let elementToDelete = found.next;
-
-        if (elementToDelete.next) {
-            found.next = elementToDelete.next;
-        } else {
-            found.next = null;
-            this.lastElement = found;
-        }
-
-    }
-
-    print() {
-        let tempElement = this.firstElement;
-        while (tempElement) {
-            console.log("--------------------");
-            console.log("NODE:" + tempElement.data);
-            let next = (tempElement.next && tempElement.next.data)
-                ? tempElement.next.data
-                : null;
-            console.log('-> next: ' + next);
-            console.log("--------------------");
-            tempElement = tempElement.next;
-        }
-    }
+  }
 
 }
 
--- test/structures/linked-list.js
+++ test/structures/linked-list.js
@@ -11,49 +11,54 @@ const LinkedList = require('../../structures/linked-list');
 
 describe('LinkedList', () => {
 
-    it('should instantiate a LinkedList', () => {
+  it('should instantiate a LinkedList', () => {
 
-        let list = new LinkedList();
-        expect(list).to.be.not.undefined;
-        expect(list.firstElement)
-            .to
-            .be
-            .eql(undefined);
+    let list = new LinkedList();
+    expect(list).to.be.not.undefined;
+    expect(list.firstElement)
+      .to
+      .be
+      .eql(undefined);
+  });
+
+  describe('methods', () => {
+
+    let list;
+
+    beforeEach(() => {
+      list = new LinkedList();
+
+      for (let i = 1; i <= 10; i++) {
+        list.add(i);
+      }
     });
 
-    describe('methods', () => {
+    it('should add item to a list', () => {
+      expect(list.firstElement.data)
+        .to
+        .be
+        .eql(1);
+      expect(list.lastElement.data)
+        .to
+        .be
+        .eql(10);
+    });
 
-        let list;
-
-        beforeEach(() => {
-            list = new LinkedList();
-
-            for (let i = 1; i <= 10; i++) {
-                list.add(i);
-            }
-        });
-
-        it('should add item to a list', () => {
-            expect(list.firstElement.data)
-                .to
-                .be
-                .eql(1);
-            expect(list.lastElement.data)
-                .to
-                .be
-                .eql(10);
-        });
-
-        it('should delete item from a list', () => {
-            list.deleteNode(10);
-            list.deleteNode(5);
-            expect(list.nodeCount())
-                .to
-                .be
-                .eql(8);
-
-        });
+    it('should delete item from a list', () => {
+      list.deleteNode(10);
+      list.deleteNode(5);
+      expect(list.nodeCount())
+        .to
+        .be
+        .eql(8);
 
     });
+
+    it('searchNodeAt', () => {
+      let node = list.searchNodeAt(5);
+      expect(node.data).to.be.eql(6);
+    });
+
+  });
 
 });
